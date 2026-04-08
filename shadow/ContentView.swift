ebb25981cf1c167353d7e1600c1b38fc48d22f6c
Joshue Mosqueda Salinas <joshuemosquedasalinas@gmail.com>
2026-04-08 00:18:29 -0600

    Initial Commit

---
+++ shadow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  shadow
+//
+//  Created by Joshue Mosqueda on 4/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ shadow/shadowApp.swift
@@ -0,0 +1,17 @@
+//
+//  shadowApp.swift
+//  shadow
+//
+//  Created by Joshue Mosqueda on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct shadowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
